2319984483cb9012b64e8874993fe6273611ebb1
Alejandra Altamiran <alejandraaltamiran10.2@gmail.com>
2024-10-07 08:12:00 -0500

    cambie el api

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,7 +13,7 @@ function Form({ callback }) {
         setError(''); // Limpiar errores anteriores
 
         try {
-            const response = await fetch('https://horoscopo-back-steel.vercel.app/', {
+            const response = await fetch('https://horoscopo-back-steel.vercel.app/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
